912dc1d5a75188f164d9aa117fe181b8914fa7e5
Galleto <victorbgalleto@gmail.com>
2023-02-09 22:58:24 -0300

    progress

--- init/main.py
+++ init/main.py
@@ -1,5 +1,17 @@
-item1 = 'Phone'
-item1_price = 100
+class Item:
+    def __init__(self): #call actions instantly
+        print('I am created!')
 
+    def calculate_total_price(self, x, y):
+        return x * y
 
-print(type(item1))
+item1 = Item()
+item1.name = 'Phone'
+item1.price = 100
+item1.quantity = 5
+item1.calculate_total_price(item1.price, item1.quantity)
+
+item2 = Item()
+item2.name = 'Laptop'
+item2.price = 1000
+item2.quantity = 3
