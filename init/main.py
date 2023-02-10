b1f71eaeaa08b57ac7f209e3fe5fba99c4e6c389
Galleto <victorbgalleto@gmail.com>
2023-02-10 01:24:40 -0300

    Initialized instances

--- init/main.py
+++ init/main.py
@@ -1,5 +1,9 @@
 class Item:
+
+    pay_rate = 0.8 # The pay rate after 20% discount
+
     def __init__(self, name: str, price: float, quantity=0): #call actions instantly
+        
         # Run validations to the recieved arguments
         assert price > 0, f'Price {price} is not greater than zero!'
         assert quantity >= 0, f'Quantity {quantity} is not greater or equal to zero!'
@@ -12,7 +16,9 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity #they were already initialized
 
-item1 = Item('Phone', 100)
+item1 = Item('Phone', 100, 1)
 item1.calculate_total_price()
 
-item2 = Item('Laptop', 1000)
+item2 = Item('Laptop', 1000, 3)
+
+print(Item.pay_rate)
