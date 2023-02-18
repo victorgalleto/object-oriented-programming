a1ac2abb9274d79fce32e29fc9e98cd6f1909daf
Galleto <victorbgalleto@gmail.com>
2023-02-18 18:25:26 -0300

    Understanding the super function

--- init/main.py
+++ init/main.py
@@ -59,7 +59,21 @@ class Item:
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 class Phone(Item):
-    pass
+
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0): #call actions instantly
+        # Call to super function to have access to all attributes / methods
+        super().__init__(
+            name, price, quantity
+        ),
+
+        # Run validations to the recieved arguments
+        assert broken_phones >= 0, f'Broken Phones {broken_phones} is not greater or equal to zero!'
+        
+        # Assign to self object
+        self.broken_phones = broken_phones
+
+        # Actions to execute
+        Item.all.append(self)
 
 item1 = Item('Phone', 100, 1)
 item1.calculate_total_price()
